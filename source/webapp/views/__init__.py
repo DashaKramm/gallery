f0131121f8554f72e290b28a4adabf42bde58b28
Dasha <grace-kramm666@gmail.com>
2024-08-31 19:59:42 +0600

    реализовала без проверки 6 этап - API для избранного + JS файл

--- source/webapp/views/__init__.py
+++ source/webapp/views/__init__.py
@@ -1,3 +1,3 @@
 from webapp.views.photos import PhotoListView, PhotoDetailView, PhotoCreateView, PhotoUpdateView, PhotoDeleteView, \
-    PhotoLinkView, PhotoDetailWithTokenView
+    PhotoLinkView, PhotoDetailWithTokenView, AddToFavorites, RemoveFromFavorites
 from webapp.views.albums import AlbumDetailView, AlbumCreateView, AlbumUpdateView, AlbumDeleteView
