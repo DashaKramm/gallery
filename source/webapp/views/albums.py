b66e430801d2a86157540a8e1cdacc56e6c5dd09
Dasha <grace-kramm666@gmail.com>
2024-08-31 18:22:09 +0600

    реализовала 4 этап - настроила права доступа

--- source/webapp/views/albums.py
+++ source/webapp/views/albums.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
@@ -5,10 +7,16 @@ from webapp.forms import AlbumForm
 from webapp.models import Album, Photo
 
 
-class AlbumDetailView(DetailView):
+class AlbumDetailView(LoginRequiredMixin, DetailView):
     model = Album
     template_name = 'albums/detail.html'
     context_object_name = 'album'
+
+    def dispatch(self, request, *args, **kwargs):
+        album = self.get_object()
+        if album.is_private and album.author != request.user:
+            raise PermissionDenied("У вас нет прав для просмотра этого альбома")
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,7 +24,7 @@ class AlbumDetailView(DetailView):
         return context
 
 
-class AlbumCreateView(CreateView):
+class AlbumCreateView(LoginRequiredMixin, CreateView):
     model = Album
     template_name = 'albums/create.html'
     form_class = AlbumForm
@@ -26,15 +34,25 @@ class AlbumCreateView(CreateView):
         return super().form_valid(form)
 
 
-class AlbumUpdateView(UpdateView):
+class AlbumUpdateView(PermissionRequiredMixin, UpdateView):
     model = Album
     form_class = AlbumForm
     template_name = 'albums/update.html'
+    permission_required = 'webapp.change_album'
+
+    def has_permission(self):
+        album = self.get_object()
+        return super().has_permission() or self.request.user == album.author
 
 
-class AlbumDeleteView(DeleteView):
+class AlbumDeleteView(PermissionRequiredMixin, DeleteView):
     model = Album
     template_name = 'albums/delete.html'
+    permission_required = 'webapp.delete_album'
+
+    def has_permission(self):
+        album = self.get_object()
+        return super().has_permission() or self.request.user == album.author
 
     def get_success_url(self):
         return reverse('webapp:index')
--- source/webapp/views/photos.py
+++ source/webapp/views/photos.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -15,13 +17,19 @@ class PhotoListView(ListView):
         return Photo.objects.filter(is_private=False).order_by('-created_at')
 
 
-class PhotoDetailView(DetailView):
+class PhotoDetailView(LoginRequiredMixin, DetailView):
     model = Photo
     template_name = 'photos/detail.html'
     context_object_name = 'photo'
 
+    def dispatch(self, request, *args, **kwargs):
+        photo = self.get_object()
+        if photo.is_private and photo.author != request.user:
+            raise PermissionDenied("У вас нет прав для просмотра этой фотографии")
+        return super().dispatch(request, *args, **kwargs)
 
-class PhotoCreateView(CreateView):
+
+class PhotoCreateView(LoginRequiredMixin, CreateView):
     model = Photo
     template_name = 'photos/create.html'
     form_class = PhotoForm
@@ -31,10 +39,15 @@ class PhotoCreateView(CreateView):
         return super().form_valid(form)
 
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(PermissionRequiredMixin, UpdateView):
     model = Photo
     form_class = PhotoForm
     template_name = 'photos/update.html'
+    permission_required = 'webapp.change_photo'
+
+    def has_permission(self):
+        photo = self.get_object()
+        return super().has_permission() or self.request.user == photo.author
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -42,9 +55,14 @@ class PhotoUpdateView(UpdateView):
         return kwargs
 
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(PermissionRequiredMixin, DeleteView):
     model = Photo
     template_name = 'photos/delete.html'
+    permission_required = 'webapp.delete_photo'
+
+    def has_permission(self):
+        photo = self.get_object()
+        return super().has_permission() or self.request.user == photo.author
 
     def get_success_url(self):
         return reverse('webapp:index')
